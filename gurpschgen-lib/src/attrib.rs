905563c1aa71b705bf658d9a3eabad0f3f4fe617
msukanen <markku.sukanen@gmail.com>
2024-07-14 15:29:48 +0300

    impl Add/Sub & AddAssign/SubAssign for AttributeValue and Attribute.

--- gurpschgen-lib/src/attrib.rs
+++ gurpschgen-lib/src/attrib.rs
@@ -1,4 +1,4 @@
-use std::{cmp::max, collections::HashMap, ops::{Add, Sub}};
+use std::{cmp::max, collections::HashMap, ops::{Add, AddAssign, Sub, SubAssign}};
 
 use crate::{misc::costly::Costly, modifier::{Modifier, ModifierValue}};
 
@@ -8,18 +8,22 @@ pub enum AttributeType {
 }
 
 #[derive(Debug, Clone)]
-pub enum Attribute {
-    DX(AttributeValue),
-    HT(AttributeValue),
-    IQ(AttributeValue),
-    ST(AttributeValue),
+pub struct AttributePayload {
+    modifiers: HashMap<Modifier, Option<ModifierValue>>,
 }
 
 #[derive(Debug, Clone)]
 pub struct AttributeValue {
     base_val: i32,
     rel_val: i32,
-    pub modifiers: HashMap<Modifier, Option<ModifierValue>>,
+}
+
+#[derive(Debug, Clone)]
+pub enum Attribute {
+    DX(AttributeValue, AttributePayload),
+    HT(AttributeValue, AttributePayload),
+    IQ(AttributeValue, AttributePayload),
+    ST(AttributeValue, AttributePayload),
 }
 
 pub trait AttributeValued {
@@ -78,7 +82,6 @@ impl Add<i32> for AttributeValue {
         Self {
             rel_val: max(-(self.base_val - 1), self.rel_val + rhs),
             base_val: self.base_val,
-            modifiers: self.modifiers,
         }
     }
 }
@@ -90,6 +93,18 @@ impl Sub<i32> for AttributeValue {
      */
     fn sub(self, rhs: i32) -> Self::Output {
         self + (-rhs)
+    }
+}
+
+impl AddAssign<i32> for AttributeValue {
+    fn add_assign(&mut self, rhs: i32) {
+        self.set_rel_val(self.rel_val + rhs);
+    }
+}
+
+impl SubAssign<i32> for AttributeValue {
+    fn sub_assign(&mut self, rhs: i32) {
+        self.set_rel_val(self.rel_val - rhs);
     }
 }
 
@@ -117,14 +132,17 @@ impl Attribute {
         let attrib_value = AttributeValue {
             base_val,
             rel_val,
+        };
+
+        let payload = AttributePayload {
             modifiers: if let Some(m) = modifiers {m} else {HashMap::new()},
         };
 
         match attrib_type {
-            AttributeType::DX => Attribute::DX(attrib_value),
-            AttributeType::HT => Attribute::HT(attrib_value),
-            AttributeType::IQ => Attribute::IQ(attrib_value),
-            AttributeType::ST => Attribute::ST(attrib_value),
+            AttributeType::DX => Attribute::DX(attrib_value, payload),
+            AttributeType::HT => Attribute::HT(attrib_value, payload),
+            AttributeType::IQ => Attribute::IQ(attrib_value, payload),
+            AttributeType::ST => Attribute::ST(attrib_value, payload),
         }
     }
 
@@ -137,24 +155,60 @@ impl Attribute {
     pub fn default(attrib_type: AttributeType) -> Self {
         Self::new(attrib_type, 10, 0, None)
     }
+
+    /**
+     Set a `modifier`.
+
+     **Params**
+     * `modifier` - some sort of a [Modifier]/[ModifierValue] pair, for which value part is optional.
+      
+     **Returns** `&self` for chaining purposes.
+     */
+    pub fn set_modifier(&mut self, modifier: (Modifier, Option<ModifierValue>)) -> &Self {
+        match self {
+            Self::DX(_, p) |
+            Self::HT(_, p) |
+            Self::IQ(_, p) |
+            Self::ST(_, p) => p.modifiers.insert(modifier.0, modifier.1),
+        };
+        self
+    }
+
+    /**
+     Unset a modifier. Nothing, of course, happens if said `modifier` is not present at all.
+
+     **Params**
+     * `modifier` - [Modifier] to unset.
+     
+     **Returns** `&self` for chaining purposes.
+     */
+    pub fn unset_modifier(&mut self, modifier: Modifier) -> &Self {
+        match self {
+            Self::DX(_, p) |
+            Self::HT(_, p) |
+            Self::IQ(_, p) |
+            Self::ST(_, p) => p.modifiers.remove(&modifier),
+        };
+        self
+    }
 }
 
 impl AttributeValued for Attribute {
     fn base_val(&self) -> i32 {
         match self {
-            Self::DX(v) |
-            Self::HT(v) |
-            Self::IQ(v) |
-            Self::ST(v) => v.base_val()
+            Self::DX(v, _) |
+            Self::HT(v, _) |
+            Self::IQ(v, _) |
+            Self::ST(v, _) => v.base_val()
         }
     }
 
     fn rel_val(&self) -> i32 {
         match self {
-            Self::DX(v) |
-            Self::HT(v) |
-            Self::IQ(v) |
-            Self::ST(v) => v.rel_val()
+            Self::DX(v, _) |
+            Self::HT(v, _) |
+            Self::IQ(v, _) |
+            Self::ST(v, _) => v.rel_val()
         }
     }
 }
@@ -163,15 +217,15 @@ impl Costly for Attribute {
     fn cost(&self) -> f64 {
         let mut cost: f64;
         match self {
-            Self::DX(v) => cost = 20.0 * v.rel_val() as f64,
-            Self::HT(v) => cost = 10.0 * v.rel_val() as f64,
-            Self::IQ(v) => cost = 20.0 * v.rel_val() as f64,
-            Self::ST(v) => {
+            Self::DX(v, _) => cost = 20.0 * v.rel_val() as f64,
+            Self::HT(v, _) => cost = 10.0 * v.rel_val() as f64,
+            Self::IQ(v, _) => cost = 20.0 * v.rel_val() as f64,
+            Self::ST(v, p) => {
                 cost = 10.0 * v.rel_val() as f64;
-                if v.modifiers.contains_key(&Modifier::NoFineManipulators) {
+                if p.modifiers.contains_key(&Modifier::NoFineManipulators) {
                     cost *= 0.6
                 }
-                if let Some(m) = v.modifiers.get(&Modifier::Size) {
+                if let Some(m) = p.modifiers.get(&Modifier::Size) {
                     match m {
                         Some(ModifierValue::I(v)) => cost *= 1.0 + 0.1 * max(-8, *v) as f64,
                         _ => ()
@@ -190,10 +244,10 @@ impl Add<i32> for Attribute {
      */
     fn add(self, rhs: i32) -> Self::Output {
         match self {
-            Self::DX(v) => Self::DX(v + rhs),
-            Self::HT(v) => Self::HT(v + rhs),
-            Self::IQ(v) => Self::IQ(v + rhs),
-            Self::ST(v) => Self::ST(v + rhs),
+            Self::DX(v, p) => Self::DX(v + rhs, p),
+            Self::HT(v, p) => Self::HT(v + rhs, p),
+            Self::IQ(v, p) => Self::IQ(v + rhs, p),
+            Self::ST(v, p) => Self::ST(v + rhs, p),
         }
     }
 }
@@ -205,17 +259,45 @@ impl Sub<i32> for Attribute {
      */
     fn sub(self, rhs: i32) -> Self::Output {
         match self {
-            Self::DX(v) => Self::DX(v - rhs),
-            Self::HT(v) => Self::HT(v - rhs),
-            Self::IQ(v) => Self::IQ(v - rhs),
-            Self::ST(v) => Self::ST(v - rhs),
+            Self::DX(v, p) => Self::DX(v - rhs, p),
+            Self::HT(v, p) => Self::HT(v - rhs, p),
+            Self::IQ(v, p) => Self::IQ(v - rhs, p),
+            Self::ST(v, p) => Self::ST(v - rhs, p),
         }
+    }
+}
+
+impl AddAssign<i32> for Attribute {
+    /**
+     `self += i32`
+     */
+    fn add_assign(&mut self, rhs: i32) {
+        match self {
+            Self::DX(v, _) |
+            Self::HT(v, _) |
+            Self::IQ(v, _) |
+            Self::ST(v, _) => *v += rhs
+        };
+    }
+}
+
+impl SubAssign<i32> for Attribute {
+    /**
+     `self += i32`
+     */
+    fn sub_assign(&mut self, rhs: i32) {
+        match self {
+            Self::DX(v, _) |
+            Self::HT(v, _) |
+            Self::IQ(v, _) |
+            Self::ST(v, _) => *v -= rhs
+        };
     }
 }
 
 #[cfg(test)]
 mod attrib_tests {
-    use crate::{attrib::AttributeValued, misc::costly::Costly};
+    use crate::{attrib::AttributeValued, misc::costly::Costly, modifier::Modifier};
 
     use super::{Attribute, AttributeType};
 
@@ -238,6 +320,24 @@ mod attrib_tests {
 
     #[test]
     fn subtraction_works() {
-        
+        let a = Attribute::default(AttributeType::DX);
+        let a = a - 2;
+        assert_eq!(-2, a.rel_val());
+        assert_eq!(-40.0, a.cost());
+    }
+
+    #[test]
+    fn rel_val_clamping_works() {
+        let a = Attribute::default(AttributeType::DX);
+        let a = a - 10;
+        assert_eq!(-9, a.rel_val());
+        assert_eq!(-180.0, a.cost());
+    }
+
+    #[test]
+    fn preset_modifier_works() {
+        let mut a = Attribute::default(AttributeType::ST);
+        a.set_modifier((Modifier::NoFineManipulators, None));
+        a += 2;
     }
 }
