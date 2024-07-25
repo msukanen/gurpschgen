f0787daf490be7d0d08b2b0650ba34e076e22362
msukanen <markku.sukanen@gmail.com>
2024-07-25 07:30:53 +0300

    added default TL 7 & 8.

--- gurpschgen-lib/src/config.rs
+++ gurpschgen-lib/src/config.rs
@@ -2,14 +2,20 @@ use crate::edition::GurpsEd;
 
 pub struct Config {
     pub edition: GurpsEd,
-    pub female_as_disadvantage: bool,
+    pub female_as_5pts_disadvantage: bool,
+    pub tl: i32,
 }
 
 impl Config {
-    pub fn default_tl3(edition: GurpsEd) -> Config {
-        Config {
-            edition,
-            female_as_disadvantage: true,
-        }
+    pub fn default_tl3(edition: GurpsEd) -> Self {
+        Self { edition, female_as_5pts_disadvantage: true, tl: 3, }
+    }
+
+    pub fn default_tl7(edition: GurpsEd) -> Self {
+        Self { edition, female_as_5pts_disadvantage: false, tl: 7, }
+    }
+
+    pub fn default_tl8(edition: GurpsEd) -> Self {
+        Self { edition, female_as_5pts_disadvantage: false, tl: 8, }
     }
 }
