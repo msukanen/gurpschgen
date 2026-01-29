526a3b8e042f3410c7f90383c0e94f1c3428cfd5
Markku Sukanen <markku.sukanen@gmail.com>
2026-01-29 04:02:16 +0200

    refactor: tl to use misc::tl::TL instead of i32, GurpsEd -> Edition; impl: Default for Config. Added some comments...

--- gurpschgen-lib/src/config.rs
+++ gurpschgen-lib/src/config.rs
@@ -1,21 +1,30 @@
-use crate::edition::GurpsEd;
+use crate::{edition::Edition, misc::tl::TL};
 
+/// Some generic configuration basics…
 pub struct Config {
-    pub edition: GurpsEd,
+    /// GURPS edition to use.
+    pub edition: Edition,
+    /// Female gender is considered a disadvantage? Before TL7 this is often the case…
     pub female_as_5pts_disadvantage: bool,
-    pub tl: i32,
+    /// Avg. tech level.
+    pub tl: TL,
+}
+
+impl Default for Config {
+    /// Get typical TL3 default.
+    fn default() -> Self {
+        Self { edition: Edition::default(), female_as_5pts_disadvantage: true, tl: 3.into() }
+    }
 }
 
 impl Config {
-    pub fn default_tl3(edition: GurpsEd) -> Self {
-        Self { edition, female_as_5pts_disadvantage: true, tl: 3, }
+    /// Get typical TL7 default.
+    pub fn default_tl7(edition: Edition) -> Self {
+        Self { edition, female_as_5pts_disadvantage: false, tl: 7.into(), }
     }
 
-    pub fn default_tl7(edition: GurpsEd) -> Self {
-        Self { edition, female_as_5pts_disadvantage: false, tl: 7, }
-    }
-
-    pub fn default_tl8(edition: GurpsEd) -> Self {
-        Self { edition, female_as_5pts_disadvantage: false, tl: 8, }
+    /// Get typical TL8 default.
+    pub fn default_tl8(edition: Edition) -> Self {
+        Self { edition, female_as_5pts_disadvantage: false, tl: 8.into(), }
     }
 }
