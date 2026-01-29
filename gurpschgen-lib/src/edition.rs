7008e1e2a7f6ca26d33d7e3a7ce052cf8618d33f
Markku Sukanen <markku.sukanen@gmail.com>
2026-01-29 04:02:46 +0200

    refactor: GurpsEd -> Edition. Impl: Default for Edition.

--- gurpschgen-lib/src/edition.rs
+++ gurpschgen-lib/src/edition.rs
@@ -1,4 +1,14 @@
-pub enum GurpsEd {
+//! Some GURPS edition-specific specs.
+pub enum Edition {
     Ed3,
     Ed4,
+}
+
+impl Default for Edition {
+    /// Get default edition.
+    /// 
+    /// Most legacy data is for [3rd Ed][Edition::Ed3], and thus we'll default to that.
+    fn default() -> Self {
+        Self::Ed3
+    }
 }
