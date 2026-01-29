a5e379cb0b7dd4752fbf209f0a4f16560d04706a
Markku Sukanen <markku.sukanen@gmail.com>
2026-01-30 01:25:58 +0200

    Added some commentary.

--- gurpschgen-lib/src/dta/read_lines.rs
+++ gurpschgen-lib/src/dta/read_lines.rs
@@ -3,15 +3,16 @@ use std::{fs::File, io::{BufRead, BufReader, Lines, Result}, path::Path};
 use encoding_rs::WINDOWS_1252;
 use encoding_rs_io::{DecodeReaderBytes, DecodeReaderBytesBuilder};
 
-/**
- Read lines from `filename`.
-
- **Returns** `Ok()` or `Err()`.
- */
+/// Read lines from `filename`.
+///
+/// # Returns
+/// `Ok`'d buffered line reader, or `Err`.
 pub fn read_lines<P>(filename: P) -> Result<Lines<BufReader<DecodeReaderBytes<File, Vec<u8>>>>>
 where P: AsRef<Path>, {
     let file = File::open(filename)?;
     
+    // Some DTA files notoriously contain WINDOWS_1252 encoded characters,
+    // and those do not comply with UTF-8, making use of a decoder a necessity.
     let decoder = DecodeReaderBytesBuilder::new()
         .encoding(Some(WINDOWS_1252))
         .build(file);
