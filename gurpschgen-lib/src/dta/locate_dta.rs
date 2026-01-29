9bb932d3e0c7544fbe2a9e62ba5fd275532a718a
Markku Sukanen <markku.sukanen@gmail.com>
2026-01-30 01:15:41 +0200

    Just some fine-tuning...

--- gurpschgen-lib/src/dta/locate_dta.rs
+++ gurpschgen-lib/src/dta/locate_dta.rs
@@ -1,10 +1,9 @@
 use std::{env, path::Path};
 
-/**
- Locate DTA/GEN etc. file(s) and change current working directory to there.
-
- **Panics** if suitable directory not found.
- */
+/// Locate DTA/GEN etc. file(s) and change current working directory to there.
+///
+/// **Panics** if no suitable directory found.
+/// 
 pub fn locate_dta(verbose: bool) {
     // where the datafiles might be lurking?
     let possible_dta_location = [
@@ -19,19 +18,15 @@ pub fn locate_dta(verbose: bool) {
         "../dta"
     ];
 
-    let mut found_dtas = false;
-    // Scan around - break early.
+    // Scan around - return early if found.
     for path in possible_dta_location {
         if env::set_current_dir(&Path::new(path)).is_ok() {
             // Found! We want to live there, too.
             let cwd = env::current_dir().unwrap();
             if verbose {println!("DTA found in {}", cwd.display());}
-            found_dtas = true;
-            break;
+            return;
         }
     }
 
-    if !found_dtas {
-        panic!("We could not locate .dta/.gen (or other such) file(s) in any (internally) specified potential locations!")
-    }
+    panic!("We could not locate .dta/.gen (or other such) file(s) in any (internally specified) potential locations!")
 }
