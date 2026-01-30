a8f9423c57432776f5e50826985697521c6af269
Markku Sukanen <markku.sukanen@gmail.com>
2026-01-30 08:46:46 +0200

    refactor epsilon values as consts instead of hardcoded here and there.

--- gurpschgen-lib/src/misc/approx.rs
+++ gurpschgen-lib/src/misc/approx.rs
@@ -1,18 +1,22 @@
+//! Various approximations.
+
+const F64_APPROX_EPSILON: f64 = 0.00001;
+const F32_APPROX_EPSILON: f32 = 0.001;
+
+/// A generic trait for approximations.
 pub trait Approx<T> {
-    /**
-     Check whether `self` ≅ `other`.
-     */
+    /// Check whether `self` ≅ `other`.
     fn approx(&self, other: T) -> bool;
 }
 
 impl Approx<f64> for f64 {
     fn approx(&self, other: f64) -> bool {
-        self - 0.000001 <= other && other <= self + 0.000001
+        self - F64_APPROX_EPSILON <= other && other <= self + F64_APPROX_EPSILON
     }
 }
 
 impl Approx<f32> for f32 {
     fn approx(&self, other: f32) -> bool {
-        self - 0.001 <= other && other <= self + 0.001
+        self - F32_APPROX_EPSILON <= other && other <= self + F32_APPROX_EPSILON
     }
 }
