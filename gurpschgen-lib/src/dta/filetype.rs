3a63535ad17ce3b208f2d06e7dc9562a104eb494
Markku Sukanen <markku.sukanen@gmail.com>
2026-01-31 23:43:55 +0200

    added modern data/file types...

--- gurpschgen-lib/src/dta/filetype.rs
+++ gurpschgen-lib/src/dta/filetype.rs
@@ -1,15 +1,33 @@
 use std::fmt::Display;
 
-pub(crate) enum LegacyFileType {
+/// Legacy SJG MakeChar file extensions.
+pub enum LegacyFileExt {
     DTA,
     GEN,
 }
 
-impl Display for LegacyFileType {
+impl Display for LegacyFileExt {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", match self {
             Self::DTA => "dta",
             Self::GEN => "gen",
+        })
+    }
+}
+
+/// Our file types.
+pub enum FileExt {
+    Data,
+    Genre,
+    Configuration,
+}
+
+impl Display for FileExt {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", match self {
+            Self::Configuration => "config",
+            Self::Data => "json",
+            Self::Genre => "genre",
         })
     }
 }
